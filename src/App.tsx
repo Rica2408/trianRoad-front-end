c374964ba546f0cfa29a52553f508e942bd7b368
Ricardo Reyes Varela <rreyesvar@gmail.com>
2022-05-26 18:30:12 -0500

    add order of destination and receiver

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { Box } from "@mui/material";
 import FormAddCar from "./components/FormAddCar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import OrderTrain from "./components/OrderTrain";
+import { Provider } from "react-redux";
+import { store } from './store'
 
 const App = () => {
   return (
-    <Box style={{ width: '100%'}}>
-      <FormAddCar />
-    </Box>
+    <Provider store={store}>
+      <Box style={{ width: '100%'}}>
+        <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FormAddCar />}></Route>
+          <Route path="/order" element={<OrderTrain />}></Route>
+        </Routes>
+      </BrowserRouter>
+      </Box>
+    </Provider>
   );
 }
 
